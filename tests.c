6bf363cbc8bee95824c7395607bb3fdbacee1618
Solirs <zcauchemar@gmail.com>
2023-09-10 00:49:03 +0200

    Add recursive encoding test

--- tests.c
+++ tests.c
@@ -50,8 +50,27 @@ void structtest(){
 	free(a.str);
 	
 }
+
+void recursivetest(){
+	char b[13] = "hello world!";
+	netstring a = lns_makenetstring(b, strlen(b)); 
+	char* axa = lns_getstring(a);	
+	
+	netstring c = lns_makenetstring(a.str, a.len); 
+	lns_printnetstring(c);
+	char* axc = lns_getstring(c);
+	printf("%s\n", axc);
+		
+	/* 
+	 * Expected output:
+	 * 31 36 3a 31 32 3a 68 65 6c 6c 6f 20 77 6f 72 6c 64 21 2c 2c 
+	 * 12:hello world!,
+	*/
+	
+}
 int main(){
 	helloworldtest();
 	structtest();
+	recursivetest();
 	return 0;
 }
